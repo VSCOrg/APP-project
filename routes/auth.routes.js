d540ebe41a4d996ca9f6e9e69f606064f5a3f338
claratesmon <claraperezmontes@gmail.com>
2023-11-14 18:28:21 +0100

    step1 - auth update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -183,10 +183,11 @@ router.post("/user-edit", (req, res) => {
   //console.log("ciao", userToUpdate);
   const updatedUser = req.body;
   //console.log("hello", updatedUser);
-User.findByIdAndUpdate(userToUpdate._id, { bio: updatedUser.bio }, { userlocation: updatedUser.userlocation }, { profilePicture: updatedUser.profilePicture }, { new: true })
+User.findByIdAndUpdate(userToUpdate._id, { bio: updatedUser.bio }, { new: true })
     .then((userUpdated) => { console.log(userUpdated);
       res.render("auth/user-profile", userUpdated) })
     .catch((error) => console.log("error!!", error));
+
 })
 
 // GET /auth/feed
@@ -203,7 +204,26 @@ router.get("/feed", (req, res) => {
 
 // GET /auth/post-create
 router.get("/post-create", (req, res) => {
-  res.render("auth/post-create");
+
+    res.render("auth/post-create");
+
+});
+
+router.post("/post-create", (req, res) => {
+  const user = req.session.currentUser
+  const { title, foodImage, description, expiringDate, pickUpTime, pickUpPlace, foodType, alergies } = req.body; /// from the form
+  
+  console.log("this is the req.body", req.body)
+  Foodpost.create(req.body)  //This already updates the Mongo database
+  .then((newFoodPost) =>{
+    console.log("new post created", newFoodPost)
+    // Foodpost.insertMany(newFoodPost)
+   return User.findByIdAndUpdate(user.id, { $push: { foodPosts: newFoodPost._id} })
+  })
+  .then((userUpdated) => { 
+    console.log(userUpdated)
+    res.redirect("/auth/feed");   //slash needed wehn redirecting
+  })
 });
 
 // GET /auth/post-edit
