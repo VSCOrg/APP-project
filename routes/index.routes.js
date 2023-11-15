c39aeedfe588848c18cf8e61294ad54558355111
Victor <victor.castanos@gmail.com>
2023-11-15 17:03:38 +0100

    login corrected

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const isLoggedIn = require('../middleware/isLoggedIn');
+const isLoggedOut = require('../middleware/isLoggedOut');
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", /* isLoggedIn, isLoggedOut,  */(req, res, next) => {
   let data = {
     layout: false
   }
-  res.render("index", data);
+  if(req.session.currentUser){
+    res.redirect("auth/feed"); 
+  } else{
+    res.render("index", data);
+  }
+
 });
 
 module.exports = router;
