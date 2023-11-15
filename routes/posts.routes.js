5b089338054ba1b9dfc431f8a9dd6dd221bf5b84
John Doe <johndoe@example.com>
2023-11-15 16:56:37 +0100

    css modify

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -25,11 +25,12 @@ router.get("/post-create", (req, res) => {
 router.post("/post-create", fileUploader.single('foodImage'), (req, res) => {
     const user = req.session.currentUser
     const postCreated = req.body
+    const dayFormated = new Date(postCreated.expiringDate)
     Foodpost.create({
         title: postCreated.title,
         foodImage: req.file.path,
         description: postCreated.description,
-        expiringDate: postCreated.expiringDate,
+        expiringDate: dayFormated.getDay(), 
         pickUpTime: postCreated.pickUpTime,
 
         pickUpPlace: postCreated.pickUpPlace,
